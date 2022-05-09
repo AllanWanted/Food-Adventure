04ca79a73180e6e991bf436028fca3fa499b9342
Gene02 <genesisparrilez1@gmail.com>
2022-05-09 14:44:43 -0600

    Escena funcionando

--- Assets/Scripts/Meta.cs
+++ Assets/Scripts/Meta.cs
@@ -6,14 +6,15 @@ using UnityEngine.UI;
 public class Meta : MonoBehaviour
 {
     public GameObject canvas1;
-    public Text txtplayer1;
+    public Text txtplayer;
 
-    private void OnTriggerEnter(Collider player1)
+    private void OnTriggerEnter(Collider Player)
     {
-        if (player1.name == "triggermeta")
+        if (Player.name == "triggermeta")
         {
+            Debug.Log("Colisionando");
             canvas1.SetActive(true);
-            txtplayer1.text = "Ganaste";
+            txtplayer.text = "Ganaste";
         }
     }
 }
